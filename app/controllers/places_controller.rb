faabc508e7295191292bb39a25e87d002eb85d91
Joshua Varghese <joshuavarghese527@gmail.com>
2016-04-13 13:25:03 +0000

    Allow destroying of places only by the user who created them

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-   before_actibefore_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   def index
     @places = Place.all
   end
@@ -29,13 +29,17 @@ def update
    if @place.user != current_user
     return render text: 'Not Allowed', status: :forbidden
   end
-  
+
   @place.update_attributes(place_params)
   redirect_to root_path
 end
 
 def destroy
   @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render text: 'Not Allowed', status: :forbidden
+  end
+
   @place.destroy
   redirect_to root_path
 end
