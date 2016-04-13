b2356f0223b579a324f37be519ef23c63434de91
Joshua Varghese <joshuavarghese527@gmail.com>
2016-04-13 13:29:54 +0000

    Validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
+     @place = current_user.places.create(place_params)
+  if @place.valid?
     redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
   end
 
   def show
@@ -31,6 +35,11 @@ def update
   end
 
   @place.update_attributes(place_params)
+   if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
   redirect_to root_path
 end
 
